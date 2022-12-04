0a1d724f2a0d09c73535868510459595595dfd65
refaldy04 <refaldybanggana@gmail.com>
2022-12-04 08:28:21 +0700

    hello world program

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express, { Application, Request, Response, NextFunction } from 'express'
 const app: Application = express()
 const port: Number = 4000
 
-app.use('/health', (req: Request, res: Response, next: NextFunction) => res.status(200).send({ status: 200 }))
+app.use('/health', (req: Request, res: Response, next: NextFunction) =>
+  res.status(200).send({ status: 200, data: 'hello world' })
+)
 
 app.listen(port, () => console.log(`Server is Listening on Port ${port} :)`))
